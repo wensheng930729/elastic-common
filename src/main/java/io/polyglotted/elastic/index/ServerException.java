1eb8b34ac3c2007ed25771e076cea6c53bad431b
Shankar Vasudevan <shankar@steel-eye.com>
2018-04-11 17:38:50 +0100

    move and remove

--- src/main/java/io/polyglotted/elastic/index/ServerException.java
+++
@@ -1,9 +0,0 @@
-package io.polyglotted.elastic.index;
-
-import io.polyglotted.elastic.client.ElasticException;
-
-@SuppressWarnings({"Serial"}) class ServerException extends ElasticException {
-    private ServerException(String message) { super(message); }
-
-    static ServerException serverException(String message) { return new ServerException(message); }
-}
